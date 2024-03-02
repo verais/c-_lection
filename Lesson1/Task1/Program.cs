63520d0009f0b6ce9f3116dcd5ee566b9c0698fa
Yana <petenyova2018@gmail.com>
2024-03-02 14:53:25 +0300

    add some variables lessom1:task1

--- Lesson1/Task1/Program.cs
+++ Lesson1/Task1/Program.cs
@@ -1,1 +1,11 @@
-﻿Console.WriteLine("Hello world");
+﻿// Console.WriteLine("Hello world");
+
+int value = 10;
+Console.WriteLine(value);
+
+double d = 5.55;
+char c = '!';
+bool b = true;
+Console.WriteLine(d);
+Console.WriteLine(c);
+Console.WriteLine(b);
