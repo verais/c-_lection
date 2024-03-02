e9f8a3f78c47af22ea8a6aa274337e787b9462bb
Yana <petenyova2018@gmail.com>
2024-03-02 15:33:36 +0300

    add lesson1;task4

---
+++ Lesson1/Task4/Program.cs
@@ -0,0 +1,26 @@
+﻿int a = 8;
+int b = 3;
+int c = 10;
+int d = 7;
+int e = 4;
+
+int max = a;
+
+if (b > max)
+{
+    max = b;
+}
+if (c > max)
+{
+    max = c;
+}
+if (d > max)
+{
+    max = d;
+}
+if (e > max)
+{
+    max = e;
+}
+
+Console.WriteLine(max);
